711a0c7057bfad443b63ad85dd53f33dd234e491
rhushihebbar07 <rhushihebbar22@gmail.com>
2026-09-04 22:07:03 +0530

    Update campus map and UI

--- src/data/campusData.js
+++ src/data/campusData.js
@@ -5,102 +5,161 @@ export const campusData = [
     category: "Venue",
     description: "Nitte, SH1, Karkala, Karnataka 574110",
     googleMapLink: "https://maps.app.goo.gl/v8JkvcoGG9CTSgCi6",
-    coordinates: { x: 50, y: 50 },
+
+    coordinates: {
+      lat: 13.1830026,
+      lng: 74.933989,
+    },
   },
+
   {
     id: "smv-block",
     name: "SMV Block, NMAMIT",
     category: "Academic",
     description: "5WMM+2CC NMAMIT, Nitte, Kalya, Karnataka 574110",
     googleMapLink: "https://maps.app.goo.gl/hRM8cCqE9k74bPdo9",
-    coordinates: { x: 40, y: 35 },
+
+    coordinates: {
+      lat: 13.1825612,
+      lng: 74.9335309,
+    },
   },
+
   {
     id: "rajaram-block",
     name: "Rajaram Block (2nd floor Lab 1-4)",
     category: "Academic",
     description: "Rajaram Block, 2nd Floor, Labs 1-4, NMAMIT, Nitte",
     googleMapLink: "https://maps.app.goo.gl/D42nDNvccfiKiMAH6",
-    coordinates: { x: 70, y: 40 },
+
+    coordinates: {
+      lat: 13.1825919,
+      lng: 74.9342767,
+    },
   },
+
   {
     id: "mca-seminar",
     name: "MCA Seminar Hall (4th floor)",
     category: "Venue",
     description: "MCA Seminar Hall, 4th Floor, Ramanujan Block",
     googleMapLink: "https://maps.app.goo.gl/hFy42rDjDZDVHJqT8",
-    coordinates: { x: 60, y: 25 },
+
+    coordinates: {
+      lat: 13.182866,
+      lng: 74.934351,
+    },
   },
+
   {
     id: "sambhram-hall",
     name: "Sambhram Hall",
     category: "Venue",
-    description: "5WMM+7M4, SH1, Kalya, Karnataka 574110",
+    description: "Basement of S. Ramanujan Block, NMAMIT",
     googleMapLink: "https://maps.app.goo.gl/UjwFAjqsFrvBHXpPA",
-    coordinates: { x: 30, y: 30 },
+
+    coordinates: {
+      lat: 13.1831332,
+      lng: 74.9341983,
+    },
   },
+
   {
     id: "boys-hostel",
     name: "Boy Main Hostel",
     category: "Amenities",
     description: "Boys Main Hostel, NMAMIT, Nitte",
     googleMapLink: "https://maps.app.goo.gl/vLD4pa9eMDnzTYu19",
-    coordinates: { x: 20, y: 70 },
+
+    coordinates: {
+      lat: 13.183682,
+      lng: 74.938675,
+    },
   },
+
   {
     id: "shambhavi-hall",
     name: "Shambhavi Hall",
     category: "Venue",
-    description: "Shambhavi Hall, NMAMIT, Nitte",
+    description: "1st Floor, APJ Kalam Block, NMAMIT",
     googleMapLink: "https://maps.app.goo.gl/njtVDc6FDEdtsfMt6",
-    coordinates: { x: 80, y: 60 },
+
+    coordinates: {
+      lat: 13.183356,
+      lng: 74.933679,
+    },
   },
+
   {
     id: "sowparnika-hall",
     name: "Sowparnika Hall",
     category: "Venue",
-    description: "Sowparnika Hall, NMAMIT, Nitte",
+    description: "1st Floor, S. Ramanujan Block, NMAMIT",
     googleMapLink: "https://maps.app.goo.gl/QSAr5cBbBgG4yZ3w7",
-    coordinates: { x: 75, y: 55 },
+
+    coordinates: {
+      lat: 13.182741,
+      lng: 74.934410,
+    },
   },
+
   {
     id: "atal-block",
     name: "Atal Block",
     category: "Academic",
     description: "Atal Block, NMAMIT, Nitte",
     googleMapLink: "https://maps.app.goo.gl/Qjor3s12sPd1PiB98",
-    coordinates: { x: 45, y: 20 },
+
+    coordinates: {
+      lat: 13.182631,
+      lng: 74.934819,
+    },
   },
+
   {
     id: "bc-alva-indoor",
     name: "B C Alva Memorial Indoor Stadium",
     category: "Recreation",
-    description: "5WMQ+2GC, near NET Gents hostel",
+    description: "B.C. Alva Memorial Indoor Stadium, NMAMIT",
     googleMapLink: "https://maps.app.goo.gl/1moKKSW89D2NW1PJ6",
-    coordinates: { x: 30, y: 80 },
+
+    coordinates: {
+      lat: 13.1825634,
+      lng: 74.9387522,
+    },
   },
+
   {
     id: "sanmathi",
     name: "Sanmathi",
     category: "Amenities",
-    description: "Sanmathi Cafe and Park (K B)",
+    description: "Sanmathi Cafe and Park (K B), NMAMIT",
     googleMapLink: "https://maps.app.goo.gl/BkUXs4hrhzR5W7816",
-    coordinates: { x: 50, y: 65 },
+
+    coordinates: {
+      lat: 13.182213,
+      lng: 74.9379792,
+    },
   },
+
   {
     id: "bc-alva-sports",
     name: "B.C Alva Sports Complex",
     category: "Recreation",
     description: "5WJP+343, Nitte, Karnataka 574110",
     googleMapLink: "https://maps.app.goo.gl/B41EqoZDRdr8KevQA",
-    coordinates: { x: 20, y: 90 },
-  }
-];
+
+    coordinates: {
+      lat: 13.1801468,
+      lng: 74.9352829,
+    },
+  },
+]
 
 export const facilityCategories = [
   "All",
   "Venue",
   "Academic",
   "Amenities",
-  "Recreation"
-];
+  "Recreation",
+]
